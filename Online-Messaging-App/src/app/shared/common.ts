1c8b56a61e023291fcc5228abb6bfaef5db0ba85
Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
2019-11-13 09:47:46 -0800

    Backend (#16)
    * add git ignore
    
    * add git ignore globally
    
    * updates
    
    * updates
    
    * Frontend (#10)
    
    * add codeowners
    
    * added authentication service
    
    * added registration functionality
    
    * updates
    
    * update
    
    * updates
    
    * Feature/frontend/add login component/d baiko (#9)
    
    * add login finctionality
    
    * add login-logout functionality | added common class
    
    * added helmet-csp to fix dependency issue (#15)

--- Online-Messaging-App/src/app/shared/common.ts
+++ Online-Messaging-App/src/app/shared/common.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {AuthenticationService} from "./authentication.service";
 import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
@@ -23,3 +24,30 @@ export class Common {
   }
 
 }
+=======
+import {AuthenticationService} from "./authentication.service";
+import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
+import {Constants} from "../config/app-config";
+
+@Injectable()
+export class Common {
+
+  private auth = new AuthenticationService();
+
+  constructor(private router: Router){}
+
+  public checkIfLoggedIn(){
+    return this.auth.isLoggedIn();
+  }
+
+  public moveToHome(){
+    this.routeTo(Constants.HOME_ROUTE);
+  }
+
+  public routeTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+}
+>>>>>>> ec5d5c7a87618041771489b80593741fbba826c8
