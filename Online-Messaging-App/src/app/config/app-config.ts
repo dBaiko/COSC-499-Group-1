fa9b53b785daeec7d514974f2028b8c77caa187b
dBaiko <dylanbaiko1@hotmail.com>
2019-11-12 17:25:43 -0800

    add git ignore

--- Online-Messaging-App/src/app/config/app-config.ts
+++ Online-Messaging-App/src/app/config/app-config.ts
@@ -2,3 +2,13 @@ export const CognitoConfig = {
   UserPoolId: 'ca-central-1_LYb5qiCzP',
   ClientId: '5mlgj3ilisnpvjd79ktdjg2cku'
 }
+
+export const APIConfig = {
+  RegisterAPI: 'http://localhost:8080/users/registerUser'
+}
+
+export const Constants = {
+  LOGIN_ROUTE: "/login",
+  REGISTER_ROUTE: '/register',
+  HOME_ROUTE: '/',
+}
