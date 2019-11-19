5e349663f357fb3e4d253b9d43d002e531202ff3
Karanmeet Khatra <karanmeet.khatra@gmail.com>
2019-11-19 10:39:57 -0800

    Feature/frontend/check/karanmeetkhatra10 (#20)
    * Added in Material Design, created a separate module called 'Material' where all the import statements can go. Also, am experimenting with Material in the login components
    
    * Login Page encorporated with Material Design
    
    * Encorporated Material into Register page.
    
    * Changed header to be its own component. Removed the Home,About,Services
    
    * Login is now centered. Title is changed. Hints are taken away. Register does not include DOB
    
    * Fixed issue with Material errors popping up on Login page
    
    * Changed flow of registration page. Also, fixed issue with hints not showing up before clicking

--- Online-Messaging-App/src/app/login/login-form.component.ts
+++ Online-Messaging-App/src/app/login/login-form.component.ts
@@ -1,26 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthenticationService} from "../shared/authentication.service";
 import {CommonService} from "../shared/common.service";
 
 @Component({
-  selector: 'login-form',
-  templateUrl: './login-form.component.html',
+  selector: 'app-login',
+  templateUrl: './login.component.html',
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
-
-  constructor(public common: CommonService, private auth: AuthenticationService) {
-  }
+  constructor(public common: CommonService, private auth: AuthenticationService) { }
 
   ngOnInit() {
   }
 
-  loginSubmit(form: NgForm): void {
+  loginSubmit(form: NgForm) {
     this.login(form.value.username, form.value.password);
   }
 
-  login(username: string, password: string): void {
+  login(username, password) {
     this.auth.login(username, password).subscribe(
       (data) => {
         console.log(data);
@@ -29,6 +27,7 @@ export class LoginFormComponent implements OnInit {
       (err) => {
         console.log(err);
       }
+
     );
   }
 
