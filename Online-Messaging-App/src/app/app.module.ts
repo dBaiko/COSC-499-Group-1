b2085c6e219ab6057b53ce8fb3d52acfba213f56
coronablazex <wiliamd.great@gmail.com>
2019-11-24 15:16:24 -0800

    SIDEBAR WORKS kinda

--- Online-Messaging-App/src/app/app.module.ts
+++ Online-Messaging-App/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RegisterFormComponent} from './register/register-form.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {LoginFormComponent} from './login/login-form.component';
 import {RegisterRedirectComponent} from './login/register-redirect/register-redirect.component';
 import {HomeComponent} from './home/home.component';
@@ -14,8 +14,10 @@ import {CommonService} from "./shared/common.service";
 import {AuthenticationService} from "./shared/authentication.service";
 import {HeaderComponent} from "./home/header/header.component";
 import {MaterialModule} from "./material/material.module";
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { FooterComponent } from './home/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
@@ -34,6 +36,7 @@ import { FooterComponent } from './footer/footer.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [AuthenticationService, CommonService],
