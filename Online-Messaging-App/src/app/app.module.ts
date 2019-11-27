9853d1be874465cd66ac8ba4f832f1ca2477cb20
Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
2019-11-27 00:59:13 -0800

    Feature/frontend/sidebar alt (#45)
    * 	new file:   src/app/footer/footer.component.html
    	new file:   src/app/footer/footer.component.scss
    	new file:   src/app/footer/footer.component.spec.ts
    	new file:   src/app/footer/footer.component.ts
    	new file:   src/app/sidebar/sidebar.component.html
    	new file:   src/app/sidebar/sidebar.component.scss
    	new file:   src/app/sidebar/sidebar.component.spec.ts
    	new file:   src/app/sidebar/sidebar.component.ts
    
    * test
    
    * SIDEBAR WORKS kinda
    
    * sidebar just needs to extend all the way to the bottom now
    
    * sidebar works
    
    * sidebar works fully
    
    * sidebar works fully
    
    * one test works
    
    * test case testing
    
    * tabs
    
    * sidebar in progress
    
    * sidebar done
    
    * sidebar finished
    
    * removed white space between sidenav and chatbox

--- Online-Messaging-App/src/app/app.module.ts
+++ Online-Messaging-App/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
--- Online-Messaging-App/src/app/home/sidebar/sidebar.component.ts
+++ Online-Messaging-App/src/app/home/sidebar/sidebar.component.ts
@@ -6,11 +6,28 @@ import {FormGroup, FormBuilder} from '@angular/forms';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-   opened = true;
+  opened = true;
+  publicChannelSelect: boolean = true;
+  privateChannelSelect: boolean = false;
+  friendChannelSelect: boolean = false;
 
 constructor(){}
 
   ngOnInit() {
   }
-
+selectPublicChannel():void {
+  this.publicChannelSelect = true;
+  this.privateChannelSelect= false;
+  this.friendChannelSelect= false;
+}
+selectPrivateChannel():void {
+  this.publicChannelSelect = false;
+  this.privateChannelSelect = true;
+  this.friendChannelSelect = false;
+}
+selectFriend():void {
+  this.publicChannelSelect = false;
+  this.privateChannelSelect = false;
+  this.friendChannelSelect = true;
+}
 }
