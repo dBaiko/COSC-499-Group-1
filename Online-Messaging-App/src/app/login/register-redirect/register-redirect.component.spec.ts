3f7b51fd3da379ae7caeda602798e486fda26d81
Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
2020-01-16 13:56:52 -0800

    Frontend (#53)
    * fixed tests with new lazy loaded modules (#50)
    
    * quick test fix

--- Online-Messaging-App/src/app/login/register-redirect/register-redirect.component.spec.ts
+++ Online-Messaging-App/src/app/login/register-redirect/register-redirect.component.spec.ts
@@ -3,13 +3,8 @@ import {routes} from "../../app-routing.module";
 
 import {RegisterRedirectComponent} from './register-redirect.component';
 import {RouterTestingModule} from "@angular/router/testing";
-import {HomeComponent} from "../../home/home.component";
-import {HeaderComponent} from "../../home/header/header.component";
 import {MaterialModule} from "../../material/material.module";
-import {SidebarComponent} from "../../home/sidebar/sidebar.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ChatboxComponent} from "../../home/chatbox/chatbox.component";
-import {LogoutFormComponent} from "../../home/logout/logout-form.component";
 import {HomeModule} from "../../home/home.module";
 
 describe('RegisterRedirectComponent', () => {
