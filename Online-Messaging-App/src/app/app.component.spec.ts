6b3a28e4eee7c89d2d0d6df3309b5c04c39c419e
dBaiko <dylanbaiko1@hotmail.com>
2020-01-16 13:31:14 -0800

    quick test fix

--- Online-Messaging-App/src/app/app.component.spec.ts
+++ Online-Messaging-App/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import {async, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
-import {LoginFormComponent} from "./login/login-form.component";
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
