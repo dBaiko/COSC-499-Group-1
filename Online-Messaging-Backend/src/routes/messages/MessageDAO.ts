2fb63a8d509c58bd93cefb8d5cc7d1c45b781593
dBaiko <dylanbaiko1@hotmail.com>
2020-01-20 12:45:47 -0800

    added channel specific message api

--- Online-Messaging-Backend/src/routes/messages/MessageDAO.ts
+++ Online-Messaging-Backend/src/routes/messages/MessageDAO.ts
@@ -14,12 +14,12 @@ interface MessageObject {
 
 class MessageDAO {
 
-    public getMessageHistory(): Promise<any> {
+    public getMessageHistory(channelId: number): Promise<any> {
         const params = {
             TableName: 'Messages',
             KeyConditionExpression: "channelId = :channelId",
             ExpressionAttributeValues: {
-                ":channelId": 0
+                ":channelId": channelId
             }
         };
 
@@ -29,12 +29,32 @@ class MessageDAO {
                     console.log(err);
                     reject(err);
                 } else {
-                    console.log("Query Succeeded");
-                    console.log(data);
+                    console.log("Query for " + channelId + " Succeeded");
                     resolve(data.Items);
                 }
             });
 
+        });
+
+    }
+
+    public getAllMessageHistory(): Promise<any> {
+        const params = {
+            TableName: 'Messages',
+
+        };
+
+        return new Promise((resolve, reject) => {
+            docClient.scan(params, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    console.log("Query Succeeded");
+                    resolve(data.Items.sort(
+                        (a: MessageObject, b: MessageObject) => (a.messageID > b.messageID) ? 1 : -1));
+                }
+            });
         });
 
     }
