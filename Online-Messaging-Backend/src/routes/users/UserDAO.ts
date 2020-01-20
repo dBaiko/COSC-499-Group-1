643e48acdf4ba5f5f6e0f9285d3341e03eedc8ea
dBaiko <dylanbaiko1@hotmail.com>
2020-01-20 14:36:55 -0800

    changed user registration to check for registration errors

--- Online-Messaging-Backend/src/routes/users/UserDAO.ts
+++ Online-Messaging-Backend/src/routes/users/UserDAO.ts
@@ -10,7 +10,7 @@ const table = "Users";
 
 class UserDAO {
 
-    public createNewUser(username: string, email: string, firstName: string, lastName: string): void {
+    public createNewUser(username: string, email: string, firstName: string, lastName: string): Promise<any> {
         const params = {
             Item: {
                 email,
@@ -21,12 +21,16 @@ class UserDAO {
             TableName: table
         };
 
-        docClient.put(params, (err, data) => {
-            if (err) {
-                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("Added item:", JSON.stringify(data, null, 2));
-            }
+        return new Promise((resolve, reject) => {
+            docClient.put(params, (err, data) => {
+                if (err) {
+                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                    reject(err);
+                } else {
+                    console.log("Added item:", JSON.stringify(data, null, 2));
+                    resolve();
+                }
+            });
         });
 
     }
--- Online-Messaging-Backend/src/routes/users/users.ts
+++ Online-Messaging-Backend/src/routes/users/users.ts
@@ -8,8 +8,13 @@ router.use(bodyParser());
 
 router.post('/', (req, res) => {
     const userRegistration = new UserDAO();
-    userRegistration.createNewUser(req.body.username, req.body.email, req.body.firstName, req.body.lastName);
-    res.status(200).send({status: 200, data: {message: "New user added successfully"}});
+    userRegistration.createNewUser(req.body.username, req.body.email, req.body.firstName, req.body.lastName)
+        .then(() => {
+            res.status(200).send({status: 200, data: {message: "New user added successfully"}});
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
 });
 
 export = router;
