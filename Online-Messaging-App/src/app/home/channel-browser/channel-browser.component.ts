5532cc8293dcabd303dfc17085ba74dec85eeadc
coronablazex <wiliamd.great@gmail.com>
2020-01-23 00:06:06 -0800

    fix some UI issue, and implemented temporary filter function on channel browser

--- Online-Messaging-App/src/app/home/channel-browser/channel-browser.component.ts
+++ Online-Messaging-App/src/app/home/channel-browser/channel-browser.component.ts
@@ -7,7 +7,8 @@ import {FormGroup} from "@angular/forms";
   styleUrls: ['./channel-browser.component.scss'],
 })
 export class ChannelBrowserComponent implements OnInit {
-    channels;
+    channels =[1,2,3,4,5,6,7];
+    search;
   constructor() { }
 
   ngOnInit() {
@@ -15,7 +16,16 @@ export class ChannelBrowserComponent implements OnInit {
   }
 
     sendQuery(messageForm: NgForm) {
+      //deletes the channel that matches the search criteria for now
+    for (let i in this.channels) {
+    if(this.channels[i].toString().includes(this.search.toString()))
+        this.channels.splice(Number(i),1);
+    }
+    }
 
-    this.channels= [1, 2, 3, 4, 5, 6];
+    onKey($event: KeyboardEvent) {
+      //set search value as whatever is entered on search bar every keystroke
+        this.search= ($event.target as HTMLInputElement).value;
+
     }
 }
