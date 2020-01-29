41c6de6be4189f6f181e428720e6661bbf4af82b
WSetiawan <wiliamd.great@gmail.com>
2020-01-29 04:27:10 -0800

    Feature/frontend/testing/dbaiko will (#79)
    * fixed tests with new lazy loaded modules (#50)
    
    * quick test fix
    
    * Fix/frontend/fix alphnum bug/d baiko (#63)
    
    * Backend (#62)
    
    * npm update (#56)
    
    * cleaned up code (#58)
    
    * fixed dynamodb query issue (#59)
    
    * Feature/backend/develop channel apis/d baiko (#61)
    
    * added channel specific message api
    
    * add basic get channel api's
    
    * added add new channel post api
    
    * changed user registration to check for registration errors
    
    * added create channel api with add to userchannel table
    
    * fixed bug
    
    * fixed api syntax
    
    * Feature/frontend/channelnavigation/karanmeetkhatra10 (#69)
    
    * Added in sidebar functionality with chatbox. Clicking on a channel directs user to the appropriate channel
    
    * Added in selecting channels on public,private and friends tabs. Actual channel id is being used instead of a dummy channel id
    
    * Feature/frontend/testing/will (#71)
    
    * quick test fix
    
    * quick test fix
    
    * Worked on channel browser, still some UI issue that needs fixing
    
    * Added missing files from last commit
    
    * fix some UI issue, and implemented temporary filter function on channel browser
    
    * fixed channel browser filtering
    
    * fixed button to go between channel browser and messages. Also fixed filter function should now work with any change within the search bar
    
    * implemented join channel function
    
    Co-authored-by: WSetiawan <wiliamd.great@gmail.com>
    
    * Feature/frontend/profile/will (#75)
    
    * toggle works
    
    * work in progress
    
    * optimized switch display code
    
    * cleanup
    
    * implemented profile page
    
    * reformat code
    
    * work in progress
    
    * fixed profile html
    
    * fixed styling
    
    * fiex styling
    
    Co-authored-by: Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
    
    * started doing tests
    
    * implemented testing
    
    Co-authored-by: Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
    Co-authored-by: Karanmeet Khatra <karanmeet.khatra@gmail.com>

--- Online-Messaging-App/src/app/shared/form-validation.service.spec.ts
+++ Online-Messaging-App/src/app/shared/form-validation.service.spec.ts
@@ -3,15 +3,20 @@ import { TestBed } from "@angular/core/testing";
 import { FormValidationService } from "./form-validation.service";
 
 describe("FormValidatorService", () => {
+
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [FormValidationService]
         });
         TestBed.configureTestingModule({});
+
     });
 
     it("should be created", () => {
         const service: FormValidationService = TestBed.get(FormValidationService);
         expect(service).toBeTruthy();
     });
+
+
 });
