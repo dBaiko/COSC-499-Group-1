75408ea6fec98d726170aa232b11bdf6d9c0139d
dBaiko <dylanbaiko1@hotmail.com>
2020-01-29 03:07:47 -0800

    Added mock db definition

--- Online-Messaging-Backend/src/_tests_/jest-dynamodb-config.ts
+++ Online-Messaging-Backend/src/_tests_/jest-dynamodb-config.ts
@@ -2,15 +2,55 @@ module.exports = {
     tables: [
         {
             TableName: `Users`,
-            KeySchema: [{AttributeName: "UserID", KeyType: "HASH"}],
-            AttributeDefinitions: [{AttributeName: "UserID", AttributeType: "string"}],
-            ProvisionedThroughput: {ReadCapacityUnits: 1, WriteCapacityUnits: 1},
+            KeySchema: [{ AttributeName: "username", KeyType: "HASH" }],
+            AttributeDefinitions: [
+                { AttributeName: "email", AttributeType: "S" },
+                { AttributeName: "firstName", AttributeType: "S" },
+                { AttributeName: "lastName", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 }
         },
-        // etc
-    ],
+        {
+            TableName: `Messages`,
+            KeySchema: [
+                { AttributeName: "channelId", KeyType: "HASH" },
+                { AttributeName: "insertTime", KeyType: "RANGE" }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: "content", AttributeType: "S" },
+                { AttributeName: "messageId", AttributeType: "S" },
+                { AttributeName: "username", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 }
+        },
+        {
+            TableName: `Channel`,
+            KeySchema: [
+                { AttributeName: "channelId", KeyType: "HASH" },
+                { AttributeName: "channelType", KeyType: "RANGE" }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: "channelType", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 }
+        },
+        {
+            TableName: `UserChannel`,
+            KeySchema: [
+                { AttributeName: "username", KeyType: "HASH" },
+                { AttributeName: "channelId", KeyType: "RANGE" }
+            ],
+            AttributeDefinitions: [
+                { AttributeName: "channelType", AttributeType: "S" },
+                { AttributeName: "channelName", AttributeType: "S" },
+                { AttributeName: "userChannelRole", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 }
+        }
+    ]
 
 };
-const {DocumentClient} = require("aws-sdk/clients/dynamodb");
+const { DocumentClient } = require("aws-sdk/clients/dynamodb");
 
 const isTest = process.env.JEST_WORKER_ID;
 const config = {
@@ -18,8 +58,8 @@ const config = {
     ...(isTest && {
         endpoint: "localhost:8000",
         sslEnabled: false,
-        region: "local-env",
-    }),
+        region: "local-env"
+    })
 };
 
 const ddb = new DocumentClient(config);
