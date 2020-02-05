064f2432e12ec09bb31dd3f4add5a86bde536ef3
coronablazex <wiliamd.great@gmail.com>
2020-02-04 21:18:02 -0800

    Made header show logged in username, put logout button in a dropdown

--- Online-Messaging-App/src/app/home/header/header.component.ts
+++ Online-Messaging-App/src/app/home/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService } from "../../shared/authentication.service";
+import {APIConfig, Constants} from "../../shared/app-config";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: "app-header",
@@ -8,11 +10,26 @@ import { AuthenticationService } from "../../shared/authentication.service";
 })
 export class HeaderComponent implements OnInit {
     userLoggedIn = false;
+    user;
+    private usersAPI = APIConfig.usersAPI;
 
-    constructor(private auth: AuthenticationService) {
+    constructor(private auth: AuthenticationService, private http: HttpClient) {
         this.userLoggedIn = auth.isLoggedIn();
     }
 
     ngOnInit(): void {
+        if (this.userLoggedIn==true)
+        this.getUserInfo();
+    }
+
+    getUserInfo(): void {
+        this.http.get(this.usersAPI + this.auth.getAuthenticatedUser().getUsername(), Constants.HTTP_OPTIONS).subscribe(
+            (data) => {
+                this.user = data;
+            },
+            (err) => {
+                console.log(err);
+            }
+        );
     }
 }
