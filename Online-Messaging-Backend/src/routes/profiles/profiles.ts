0418c652678bf963422454c5dbcb038483311430
dBaiko <dylanbaiko1@hotmail.com>
2020-02-20 20:33:11 -0800

    added jwt verification to profiles

--- Online-Messaging-Backend/src/routes/profiles/profiles.ts
+++ Online-Messaging-Backend/src/routes/profiles/profiles.ts
@@ -3,9 +3,12 @@ import express from "express";
 import ProfileDAO from "./ProfileDAO";
 import aws from "aws-sdk";
 import { awsConfigPath } from "../../config/aws-config";
+import { HTTPResponse, JwtVerificationService } from "../../shared/jwt-verification-service";
 
 aws.config.loadFromPath(awsConfigPath);
 const docClient = new aws.DynamoDB.DocumentClient();
+
+const jwtVerificationService: JwtVerificationService = JwtVerificationService.getInstance();
 
 const router = express.Router();
 
@@ -13,34 +16,55 @@ router.use(bodyParser());
 
 const PATH_PUT_PROFILE: string = "/";
 const PATH_GET_PROFILE: string = "/:username";
+const AUTH_KEY = "authorization";
 
 router.put(PATH_PUT_PROFILE, (req, res) => {
-    const updateProfile = new ProfileDAO(docClient);
-    updateProfile
-        .updateProfile(req.body.username, req.body.firstName, req.body.lastName)
-        .then(() => {
-            res.status(200).send({
-                status: 200,
-                data: { message: "Profile for user" + req.body.username + "updated successfully" }
-            });
-        })
-        .catch((err) => {
-            res.status(400).send(err);
-        });
+
+    let token: string = req.headers[AUTH_KEY];
+
+    jwtVerificationService.verifyJWTToken(token).subscribe(
+        (data) => {
+            const updateProfile = new ProfileDAO(docClient);
+            updateProfile
+                .updateProfile(req.body.username, req.body.firstName, req.body.lastName)
+                .then(() => {
+                    res.status(200).send({
+                        status: 200,
+                        data: { message: "Profile for user" + req.body.username + "updated successfully" }
+                    });
+                })
+                .catch((err) => {
+                    res.status(400).send(err);
+                });
+        },
+        (err: HTTPResponse) => {
+            res.status(err.status).send(err);
+        }
+    );
 });
 
 router.get(PATH_GET_PROFILE, (req, res) => {
-    const getProfile = new ProfileDAO(docClient);
-    let username = req.params.username;
 
-    const response = getProfile
-        .getUserProfile(username)
-        .then((data) => {
-            res.status(200).send(data);
-        })
-        .catch((err) => {
-            res.status(400).send(err);
-        });
+    let token: string = req.headers[AUTH_KEY];
+
+    jwtVerificationService.verifyJWTToken(token).subscribe(
+        (data) => {
+            const getProfile = new ProfileDAO(docClient);
+            let username = req.params.username;
+
+            const response = getProfile
+                .getUserProfile(username)
+                .then((data) => {
+                    res.status(200).send(data);
+                })
+                .catch((err) => {
+                    res.status(400).send(err);
+                });
+        },
+        (err: HTTPResponse) => {
+            res.status(err.status).send(err);
+        }
+    );
 });
 
 export = router;
