6f9e6e18e24042e8f748906759f04668e36203b4
Michwds <michael.woods@live.ca>
2020-03-17 13:49:07 -0700

    added sidebar for online users in a channel, WIP.  New Filed added

---
+++ Online-Messaging-App/src/app/home/chatbox/channel-user-list/channel-user-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-user-list',
+  templateUrl: './channel-user-list.component.html',
+  styleUrls: ['./channel-user-list.component.scss']
+})
+export class ChannelUserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
